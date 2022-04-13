bd2debcb2b23a6d29c009f57b72f5b372a9527b8
Andreas Georgiou <ag14774@users.noreply.github.com>
2022-04-13 21:31:10 +0200

    Made asset module public (#19)

--- packages/steadifi/src/lib.rs
+++ packages/steadifi/src/lib.rs
@@ -2,6 +2,5 @@ pub mod asset;
 pub mod collateral_manager;
 pub mod mars_protocol_math;
 pub mod oracle_manager;
-pub use asset::{AssetInfo, AssetInfoUnvalidated, NormalAssetInfo, NormalAssetInfoUnvalidated};
 
 mod testing;
